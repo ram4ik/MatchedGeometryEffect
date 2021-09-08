1e09ac3d90b70865f2377f457b237d0db0594001
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-08 21:12:53 +0300

    Initial Commit

---
+++ MatchedGeometryEffect/MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectApp.swift
+//  MatchedGeometryEffect
+//
+//  Created by ramil on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
